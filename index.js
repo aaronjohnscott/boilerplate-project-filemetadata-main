7b589634b2c731dee08590c0e61c19dc1201abe8
Aaron Scott <aaronjohnscott@gmail.com>
2023-04-03 09:00:39 -0700

    finished - tested

--- index.js
+++ index.js
@@ -2,6 +2,9 @@ var express = require('express');
 var cors = require('cors');
 require('dotenv').config()
 
+const multer = require('multer');
+const upload = multer({ dest: 'uploads/' });
+
 var app = express();
 
 app.use(cors());
@@ -11,7 +14,15 @@ app.get('/', function (req, res) {
   res.sendFile(process.cwd() + '/views/index.html');
 });
 
-
+app.post('/api/fileanalyse', upload.single('upfile'), (req, res) => {
+  const { originalname, mimetype, size } = req.file;
+  console.log("yo yo yo", (req.file))
+  res.json({
+    name: originalname,
+    type: mimetype,
+    size
+  })
+});
 
 
 const port = process.env.PORT || 3000;
